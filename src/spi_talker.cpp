f3ef24fe800caa5a4913f77ac98446cc10c518f0
GangstaRob <35857619+GangstaRob@users.noreply.github.com>
2018-07-31 08:20:55 -0500

    Update spi_talker.cpp

--- src/spi_talker.cpp
+++ src/spi_talker.cpp
@@ -10,26 +10,11 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-//#include "adis16480.c"
 void shellClr();
 
-int main(int argc, char *argv[]){
-
-    ros::init(argc, argv, "imu_talker");
-    ros::NodeHandle nh;
-    ROS_INFO_STREAM("hello, ROS!");
-
-__u8 tx[]={(int)strtol(argv[1], NULL, 16), (int)strtol(argv[2], NULL, 16), (int)strtol(argv[3], NULL, 16), (int)strtol(argv[4], NULL, 16)};
-
-	shellClr();
-	spiReadWrite(tx);
+int main(int argc, char **argv) {
 	
-	return 0;
-}
-
-
-void shellClr(){
-	char cmd[25];
-	snprintf(cmd,24,"clear");	
-  	int rv = system(cmd);
+	ros::init(argc, argv, "hello_ros");
+	ros::NodeHandle nh;
+	ROS_INFO_STREAM("hello, ROS!");
 }
