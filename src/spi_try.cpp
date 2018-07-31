d0235d22957a2cba5920221c79253738f757ac92
GangstaRob <35857619+GangstaRob@users.noreply.github.com>
2018-07-31 16:56:14 -0500

    Add files via upload

---
+++ src/spi_try.cpp
@@ -0,0 +1,102 @@
+#include "spi.cpp"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ros/ros.h>
+#include <sstream>
+using namespace std;
+
+string convert_int(int n) {
+    stringstream ss;
+    ss << n;
+    return ss.str();
+}
+
+void SpiWriteRead(int length, const char *device, uint8_t *msg)
+{
+// Initialize parameters
+    uint8_t mode = 0;
+    uint8_t bits = 8;
+    uint32_t speed = 32000;
+
+// Open device
+    int fd = open(device, O_RDWR);
+    if (fd < 0) {
+        ROS_ERROR("Can't open device");
+        exit(1);
+    }
+// SPI mode
+    int ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
+    if (ret < 0) {
+        ROS_ERROR("Can't set spi mode");
+        exit(1);
+    }
+// Bits per word
+    ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+    if (ret < 0) {
+        ROS_ERROR("Can't set bits per word");
+        exit(1);
+    }
+// Max speed, 1ms per 32bit command --> 32kHz
+    ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+    if (ret < 0) {
+        ROS_ERROR("Can't set max speed");
+        exit(1);
+    }
+
+    unsigned char tx[256];
+    unsigned char rx[256];
+    for (int i = 0; i < length; i++) {
+        tx[i] = 0x00;
+        rx[i] = 0x00;
+    }
+
+    printf("tx: ");
+    for(int i;i<Numel(tx);i++){
+        cout << tx[i];
+    }
+    printf("\n");
+
+    struct spi_ioc_transfer tr[256];
+    for (int i = 0; i < length; i++) {
+        tr[i].tx_buf = (unsigned long)&tx[i];
+        tr[i].rx_buf = (unsigned long)&rx[i];
+        tr[i].len = 1;
+        tr[i].delay_usecs = 1000;
+        tr[i].speed_hz = speed;
+        tr[i].bits_per_word = 8;
+    }
+    ret = ioctl(fd, SPI_IOC_MESSAGE(length), tr);
+    if (ret < 1)
+    {
+        ROS_ERROR("Error read or write - ioctl");
+        exit(1);
+    }
+    int i=0;
+    printf("rx: ");
+    for(i;i<Numel(rx);i++){
+        cout << rx[i];
+    }
+    printf("\n");
+// Close device
+    close(fd);
+}
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "spiInterface");
+    ros::NodeHandle nh;
+    ros::Duration duration(1. / 24.);
+// Length of byte stream
+    int length = 2;
+    sleep(2);
+    while (ros::ok())
+    {
+        unsigned char rightMsg[] = { 0x00, 0x77, 0x00, 0x77, 0x00, 0x77, 0x00 };
+        const char *dev0 = "/dev/spidev1.0";
+        SpiWriteRead(length, dev0, rightMsg);
+        duration.sleep();
+    }
+    return 0;
+}
